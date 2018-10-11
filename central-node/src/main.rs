19faaba87430e67103515ebc2691313320c2d461
Sehny <msehnout@users.noreply.github.com>
2018-10-11 20:53:26 +0200

    implement async request forwarding

--- central-node/src/main.rs
+++ central-node/src/main.rs
@@ -3,11 +3,15 @@ extern crate warp;
 #[macro_use]
 extern crate log;
 extern crate simple_logger;
+extern crate reqwest;
 
 use warp::Filter;
+use warp::Future;
+use reqwest::async::Client;
 
 fn main() {
     simple_logger::init_with_level(log::Level::Info).unwrap();
+    let client = warp::any().map(move || Client::new());
 
     // GET /alive => 200 OK with body "yes"
     let hello = path!("alive")
@@ -16,7 +20,24 @@ fn main() {
             "yes"
         });
 
+    let f = path!("f" / String)
+        .and(client)
+        .and_then(|arg, client: Client| {
+            info!("spawn request");
+            client
+                .get(&format!("http://{}", arg)).send()
+                .map_err(|e| {
+                    error!("{}", e);
+                    warp::reject::server_error()
+                })
+        })
+        .map(|response| format!("res: {:#?}", response));
+
+    let api = f.or(hello);
+
+    let routes = api.with(warp::log(""));
+
     info!("Starting the server");
-    warp::serve(hello)
+    warp::serve(routes)
         .run(([0, 0, 0, 0], 8080));
 }
